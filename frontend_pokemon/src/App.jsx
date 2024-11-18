dd3c9edb6cf7d7e1240aeaf94766860271a13262
tigermarg <178945478+tigermarg@users.noreply.github.com>
2024-11-17 20:43:52 -0500

    onClick function to Login page

---
+++ frontend_pokemon/src/App.jsx
@@ -0,0 +1,32 @@
+import { Routes, Route } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import './App.css';
+import pokeball from './styles/pokeball.jpg';
+import Login from './pages/Login';
+
+function App() {
+  const nav = useNavigate();
+  
+  function handleClick(){
+    nav('/login');
+  }
+
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={
+        <>
+          <h2>Your Pokémon awaits!</h2>
+          <h3>Match your mood with a Pokémon today—Click the Pokéball to see who you catch!</h3>
+          <img onClick={handleClick} src={pokeball} alt="Pokeball" style={{ width: '40vw', height: 'auto' }} />             
+        </>
+        } />
+        <Route path='/login' element={<Login />} />
+        <Route path='/dashboard' element />
+        <Route path='/mypokeball' element />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
