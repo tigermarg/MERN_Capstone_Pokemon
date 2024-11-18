fb22e7789eb70057879c2a572d4843af2b0be51d
tigermarg <178945478+tigermarg@users.noreply.github.com>
2024-11-18 11:22:16 -0500

    Working login form

--- frontend_pokemon/src/components/login/LoginForm.jsx
+++ frontend_pokemon/src/components/login/LoginForm.jsx
@@ -1,8 +1,43 @@
-function LoginForm(){
-  return(
-    <>Login Form</>
-  )
-}
+import { useState } from 'react';
 
+function LoginForm() {
+  const [formData, setFormData] = useState({ //State for formData
+    email: '',
+    password: '',
+  });
+
+  function handleChange(e){ //Handler for input
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  function handleSubmit(e){ //Handler for submit
+    e.preventDefault();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Email:</label>
+        <input 
+          type="email" 
+          onChange={handleChange} 
+          required
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input 
+          type="password" 
+          onChange={handleChange} 
+          required
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
 
 export default LoginForm;
