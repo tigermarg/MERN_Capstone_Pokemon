3929f8766cee0d4c0ac634846ddc00f413204f7f
tigermarg <178945478+tigermarg@users.noreply.github.com>
2024-11-21 15:49:15 -0500

    Logout event complete

--- frontend_pokemon/src/pages/Dashboard.jsx
+++ frontend_pokemon/src/pages/Dashboard.jsx
@@ -1,7 +1,17 @@
 //Imports
+import { useAuth } from '../contexts/auth_context';
+import { useNavigate } from 'react-router-dom';
 import NavBar from '../components/dashboard/NavBar';
 
 export default function Dashboard(){
+  const { logout } = useAuth(); 
+  const nav = useNavigate();
+
+  //Handler for log out click event
+  function handleLogOut(e){
+    logout();
+    nav('/'); //Redirect to main page at logout
+  }
 
     return(
       //Navigation bar
@@ -12,7 +22,7 @@ export default function Dashboard(){
         <p>Access to dashboard...</p>
 
         {/* Log Out button */}
-        <button onClick={() => alert('Logged Out!')}>Log Out</button>
+        <button onClick={handleLogOut}>Log Out</button>
       </div>
     )
 }
