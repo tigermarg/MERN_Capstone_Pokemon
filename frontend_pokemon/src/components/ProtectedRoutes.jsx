d56b340efafa8de223534aa5c4cc25ebfd65de23
tigermarg <178945478+tigermarg@users.noreply.github.com>
2024-11-21 14:38:26 -0500

    Created ProtectedRoutes component

---
+++ frontend_pokemon/src/components/ProtectedRoutes.jsx
@@ -0,0 +1,8 @@
+import { Outlet } from 'react-router-dom';
+import { useAuth } from '../contexts/auth_context';
+
+export default function ProtectedRoutes(){
+    const { cookies } = useAuth(); //Pulls cookies out of Auth Context
+
+    return cookies.token ? <Outlet /> : <h2>Must be signed in to view.</h2>
+}
